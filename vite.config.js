e027707eee5549de74f43d03c60e611bdba0a825
Andrew <ajuhpark@gmail.com>
2025-01-15 22:50:03 -0800

    trying just one file to build them.

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    host: 'localhost',
+    cors: true, // Use true instead of '*'. Adjust if specific CORS rules are needed.
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws',
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        homepage: 'lib/homepage.js',
+        color_mode: 'lib/color_mode_toggle.js',
+        case_study: 'lib/case_study.js',
+        cs_website_builder_tools: 'lib/cs_website_builder_tools.js',
+      },
+      output: {
+        format: 'es', // Use 'es' format for code-splitting
+        entryFileNames: '[name].js',
+        esModule: true,
+        compact: true,
+        globals: {
+          jquery: '$',
+        },
+      },
+      external: ['jquery'],
+    },
+  }
+});
